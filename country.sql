3adf5006237d119d3ddc6d3f83004d161e2876db
Buggy <kopirgan@gmail.com>
2025-05-15 17:58:27 +0530

    Add db files

---
+++ country.sql
@@ -0,0 +1,19 @@
+CREATE TABLE connections (
+    cnfd INTEGER,
+    cncrdt INTEGER,
+    cnupdt INTEGER,
+    cnon INTEGER
+);
+
+CREATE TABLE countries (			
+			
+cocou   TEXT PRIMARY KEY,
+conm1	TEXT,
+conm2   TEXT,
+cocur   TEXT,
+couse   INTEGER,
+cocrdt	INTEGER,
+coupdt	INTEGER
+
+);
+
